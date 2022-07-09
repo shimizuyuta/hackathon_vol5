8f154fce9b61088da80ff4b3c72df9fc28e71960
k20098kk <k20098kk@k20098kknoMacBook-Air-5.local>
2022-07-10 03:15:41 +0900

    heroku

--- flask/web.py
+++ flask/web.py
@@ -1,12 +1,12 @@
 from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 # from io import BytesIO
 # from PIL import Image
 
 from api.analyze import Analyze as AL
 
-# load_dotenv()
+load_dotenv()
 app = Flask(__name__)
 app.config.from_envvar('FLASK_CONFIG_FILE')
 CORS(
@@ -15,9 +15,9 @@ CORS(
 )
 
 
-@app.route("/")
+@app.route("/",methods=['POST'])
 def test():
-    return render_template('test.html')
+    return jsonify({'test':'Hello!!World!!'})
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
